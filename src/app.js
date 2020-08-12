2267658a58dcd4cd4e1149dede213c2434182136
orel <oreltol1000@gmail.com>
2020-08-12 16:56:55 +0300

    make it better

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ const partialsPath = path.join(__dirname, '../templates/partials')
 app.set('view engine', 'hbs')
 app.set('views', viewsPath)
 hbs.registerPartials(partialsPath)
-
+//heroko
 // Setup static directory to serve
 app.use(express.static(publicDirectoryPath))
 
