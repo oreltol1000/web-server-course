5f534dd257c7b978395fe58719b415b20a0f254c
orel <oreltol1000@gmail.com>
2020-08-12 17:25:29 +0300

    herokuuuu

--- public/js/app.js
+++ public/js/app.js
@@ -7,7 +7,7 @@ weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
 
     const location = search.value
-
+//
     fetch('/weather?address=' + location).then((response) => {
         response.json().then((data) => {
             if (data.error) {
